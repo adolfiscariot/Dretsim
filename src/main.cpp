6ba0d2fa6b22c7ce53868d2fdabd959e0f4433e7
adolfiscariot <m10mathenge@gmail.com>
2026-01-18 14:15:49 +0300

    feat: Create Window 4 The 1st Timegit add .git add . Plus updated gitignore to ignore build folder

--- src/main.cpp
+++ src/main.cpp
@@ -1,13 +1,46 @@
-#include <iostream>
+#include <glad/glad.h>
 #include <GLFW/glfw3.h>
+#include <iostream>
 
-int main() {
-	std::cout << "Engine starting ...\n";
+void framebuffer_size_callback(GLFWwindow* window, int width, int height){
+	glViewport(0, 0, width, height);
+}
 
-	for (int i = 0; i < 5; i++){
-		std::cout << "Frame " << i << "\n";
+int main(){
+	glfwInit();
+	glfwWindowHint(GLFW_CONTEXT_VERSION_MAJOR, 3);
+	glfwWindowHint(GLFW_CONTEXT_VERSION_MINOR, 3);
+	glfwWindowHint(GLFW_OPENGL_PROFILE, GLFW_OPENGL_CORE_PROFILE);
+
+	GLFWwindow* window = glfwCreateWindow(800, 600, "LearnOpenGL", NULL, NULL);
+	if (window == NULL){
+		std::cout << "Failed to create GLFW window";
+		glfwTerminate();
+		return -1;
 	}
 
-	std::cout << "Engine stopped\n";
+	glfwMakeContextCurrent(window);
+
+	if (!gladLoadGLLoader((GLADloadproc)glfwGetProcAddress)){
+		std::cout << "Failed to initialize GLAD";
+		return -1;
+	}
+
+	glViewport(0, 0, 800, 600);
+
+	// Resize the window
+	void framebuffer_size_callback(GLFWwindow* window, int width, int height);
+
+	// Tell GLFW to call the above function everytime on resize
+	glfwSetFramebufferSizeCallback(window, framebuffer_size_callback);
+
+	// Keep rendering until explicitly told to stop
+	while (!glfwWindowShouldClose(window)){
+		glfwSwapBuffers(window);
+		glfwPollEvents();
+	}
+
+	glfwTerminate();
 	return 0;
 }
+
