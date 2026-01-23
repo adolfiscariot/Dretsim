1e4d6a7ac0cca16c54503955d3c69ab3656276e3
adolfiscariot <m10mathenge@gmail.com>
2026-01-23 17:15:54 +0300

    feat: change background color to teal

--- src/main.cpp
+++ src/main.cpp
@@ -6,36 +6,53 @@ void framebuffer_size_callback(GLFWwindow* window, int width, int height){
 	glViewport(0, 0, width, height);
 }
 
+// Close window if user presses esc key
+void processInput(GLFWwindow *window){
+	if(glfwGetKey(window, GLFW_KEY_ESCAPE) == GLFW_PRESS){
+		glfwSetWindowShouldClose(window, true);
+	}
+}
+
 int main(){
+	// Initalize glfw library
 	glfwInit();
+
+	// State the version 3.3 and profile (core, no deprecated functions)
 	glfwWindowHint(GLFW_CONTEXT_VERSION_MAJOR, 3);
 	glfwWindowHint(GLFW_CONTEXT_VERSION_MINOR, 3);
 	glfwWindowHint(GLFW_OPENGL_PROFILE, GLFW_OPENGL_CORE_PROFILE);
 
+	// Create an opengl window and context
 	GLFWwindow* window = glfwCreateWindow(800, 600, "LearnOpenGL", NULL, NULL);
 	if (window == NULL){
 		std::cout << "Failed to create GLFW window";
 		glfwTerminate();
 		return -1;
 	}
-
 	glfwMakeContextCurrent(window);
 
+	// Retrieve glad's opengl function pointers by asking the graphics driver for their address
 	if (!gladLoadGLLoader((GLADloadproc)glfwGetProcAddress)){
 		std::cout << "Failed to initialize GLAD";
 		return -1;
 	}
 
+	// Create dimensions 
 	glViewport(0, 0, 800, 600);
-
-	// Resize the window
-	void framebuffer_size_callback(GLFWwindow* window, int width, int height);
 
 	// Tell GLFW to call the above function everytime on resize
 	glfwSetFramebufferSizeCallback(window, framebuffer_size_callback);
 
 	// Keep rendering until explicitly told to stop
 	while (!glfwWindowShouldClose(window)){
+
+		// input
+		processInput(window);
+
+		glClearColor(0.2f, 0.3f, 0.3f, 1.0f);
+		glClear(GL_COLOR_BUFFER_BIT);
+
+		// check for and call events and swap the buffers
 		glfwSwapBuffers(window);
 		glfwPollEvents();
 	}
