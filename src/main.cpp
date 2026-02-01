187e8f07753150163b8a4aeb8998ee12a501876c
Mark Mathenge <m10mathenge@gmail.com>
2026-02-01 16:39:11 +0300

    feat: create legendary red, blue and green triangle

--- src/main.cpp
+++ src/main.cpp
@@ -13,12 +13,28 @@ void processInput(GLFWwindow *window){
 	}
 }
 
+/*
+ * ===========================================================
+ * GLSL SHADER CODE
+ * ===========================================================
+ */
 const char *vertexShaderSource = "#version 330 core\n"
 	"layout (location = 0) in vec3 aPos;\n"
+	"layout (location = 1) in vec3 aColor;\n"
+	"out vec3 ourColor;\n" // output a color to the fragment shader
 	"void main()\n"
 	"{\n"
-		"   gl_Position = vec4(aPos.x, aPos.y, aPos.z, 1.0);\n"
+		"gl_Position = vec4(aPos, 1.0);\n" // Use vec3 in vec4's input
+		"ourColor = aColor;\n" // set ourColor to the input color we get from the vertex data
 	"}\0";
+
+const char *fragmentShaderSource = "#version 330 core\n"
+	"out vec4 FragColor;\n"
+	"in vec3 ourColor;\n"
+	"void main()\n"
+	"{\n"
+		"FragColor = vec4(ourColor, 1.0);\n"
+	"}\n";
 
 
 int main(){
@@ -56,34 +72,6 @@ int main(){
 	 * VERTEX SHADER
 	 * ===========================================================
 	 */
-	
-	// Rectangle co-ordinates
-	float vertices[] = {
-		 0.5f,  0.5f, 0.0f, // top right
-		 0.5f, -0.5f, 0.0f, // bottom right
-		-0.5f, -0.5f, 0.0f, // bottom left
-		-0.5f,  0.5f, 0.0f  // top left
-	};
-
-	unsigned int indices[] = {
-		0, 1, 3, // first triangle
-		1, 2, 3  // second triangle
-	};
-
-	// Create a vertex buffer object and give it an ID
-	unsigned int VBO;
-	glGenBuffers(1, &VBO);
-
-	unsigned int EBO; // Element Buffer Obj. Useful for storing indices (for multiple triangles)
-	glGenBuffers(1, &EBO);  
-
-	// Bind created buffer to type of vertex buffer
-	glBindBuffer(GL_ARRAY_BUFFER, VBO);
-	glBufferData(GL_ARRAY_BUFFER, sizeof(vertices), vertices, GL_STATIC_DRAW);
-
-	glBindBuffer(GL_ELEMENT_ARRAY_BUFFER, EBO);
-	glBufferData(GL_ELEMENT_ARRAY_BUFFER, sizeof(indices), indices, GL_STATIC_DRAW);
-
 
 	// Create a vertex shader object
 	unsigned int vertexShader;
@@ -108,18 +96,17 @@ int main(){
 	 * ===========================================================
 	 */
 	
-	const char *fragmentShaderSource = "#version 330 core\n"
-		"out vec4 FragColor;\n"
-		"void main()\n"
-		"{\n"
-			"FragColor = vec4(1.0f, 0.5f, 0.2f, 1.0f);\n"
-		"}\n";
-	
 	unsigned int fragmentShader;
 	fragmentShader = glCreateShader(GL_FRAGMENT_SHADER);
+	
 	glShaderSource(fragmentShader, 1, &fragmentShaderSource, NULL);
 	glCompileShader(fragmentShader);
 
+	glGetShaderiv(fragmentShader, GL_COMPILE_STATUS, &success);
+	if(!success){
+		glGetShaderInfoLog(fragmentShader, 512, NULL, infoLog);
+		std::cout << "ERROR::SHADER::FRAGMENT::COMPILATION_FAILED\n" << infoLog;
+	}
 
 	/*
 	 * ===========================================================
@@ -138,33 +125,50 @@ int main(){
 	glGetProgramiv(shaderProgram, GL_LINK_STATUS, &success);
 	if(!success){
 		glGetProgramInfoLog(shaderProgram, 512, NULL, infoLog);
+		std::cout << "ERROR::SHADER::PROGRAM::LINKING_FAILED\n" << infoLog;
 	} 
-
-	// Tell opengl how to interprex vector input
-	glVertexAttribPointer(0, 3, GL_FLOAT, GL_FALSE, 3 * sizeof(float), (void*)0);
-	glEnableVertexAttribArray(0);
-
-	// Activate newly created program object
-	glUseProgram(shaderProgram);
 
 	// Delete shader objects after linking. We don't need them any more
 	glDeleteShader(vertexShader);
 	glDeleteShader(fragmentShader);
 
-	// Generate a vertex attribute object and give it an ID
-	unsigned int VAO;
+	/*
+	 * ===========================================================
+	 * CONFIGURE VERTEX DATA, BUFFERS & ATTRIBUTES
+	 * ===========================================================
+	 */
+
+	// Triangle co-ordinates
+	float vertices[] = {
+		 // position         // color
+		 0.5f, -0.5f, 0.0f,  1.0f, 0.0f, 0.0f,  // bottom right
+		-0.5f, -0.5f, 0.0f,  0.0f, 1.0f, 0.0f,  // bottom left
+		 0.0f,  0.5f, 0.0f,  0.0f, 0.0f, 1.0f   //top
+	};
+
+
+	unsigned int VBO, VAO;
+
+	// Create VAO and VBO, giving each an index
 	glGenVertexArrays(1, &VAO);
+	glGenBuffers(1, &VBO);
 
-	// Bind VAO to VBO
+	// Bind & set vertex buffers
 	glBindVertexArray(VAO);
+	glBindBuffer(GL_ARRAY_BUFFER, VBO);
+	glBufferData(GL_ARRAY_BUFFER, sizeof(vertices), vertices, GL_STATIC_DRAW);
 
-	// Copy our vertices array in a buffer for OpenGL to use
-	glBindBuffer(GL_ELEMENT_ARRAY_BUFFER, EBO);
-	glBufferData(GL_ELEMENT_ARRAY_BUFFER, sizeof(indices), indices, GL_STATIC_DRAW);
-
-	// Set our vertex attribute pointers
-	glVertexAttribPointer(0, 3, GL_FLOAT, GL_FALSE, 3 * sizeof(float), (void *) 0);
+	// Configure our vertex attribute pointers
+	// position attribute
+	glVertexAttribPointer(0, 3, GL_FLOAT, GL_FALSE, 6 * sizeof(float), (void *) 0);
 	glEnableVertexAttribArray(0);
+	
+	// color attribute
+	glVertexAttribPointer(1, 3, GL_FLOAT, GL_FALSE, 6 * sizeof(float), (void*)(3 *sizeof(float)));
+	glEnableVertexAttribArray(1);
+
+	// Draw the object 
+	glUseProgram(shaderProgram);
 
 	// Keep rendering until explicitly told to stop
 	while (!glfwWindowShouldClose(window)){
@@ -175,10 +179,8 @@ int main(){
 		glClearColor(0.2f, 0.3f, 0.3f, 1.0f);
 		glClear(GL_COLOR_BUFFER_BIT);
 
-		// Draw the object 
-		glUseProgram(shaderProgram);
 		glBindVertexArray(VAO);
-		glDrawElements(GL_TRIANGLES, 6, GL_UNSIGNED_INT, 0);
+		glDrawArrays(GL_TRIANGLES, 0, 3);
 
 		// check for and call events and swap the buffers
 		glfwSwapBuffers(window);
