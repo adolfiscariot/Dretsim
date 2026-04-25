3bf9a2b57360a1cb28a4832f6e3e0767c521df1a
adolfiscariot <m10mathenge@gmail.com>
2026-04-25 20:09:48 +0300

    fix: working on uninitalized particlesCount variable

--- src/simulation.cpp
+++ src/simulation.cpp
@@ -168,6 +168,7 @@ class Simulation{
 
 		// Num of particles in gpu_buffer vector
 		const size_t get_particles_count() const{
+			std::cout << gpu_buffer.size() / 5;
 			return gpu_buffer.size() / 5;
 		}
 
